afa483ba479595f4ea1c2e20cf7271c886b147e0
EvelynNamugwanya <50854828+EvelynNamugwanya@users.noreply.github.com>
2019-05-22 16:43:29 -0400

    Add files via upload

---
+++ jik.cpp
@@ -0,0 +1,59 @@
+/******************************************************************************
+
+                             MATRIX  MULTIPLICATION
+*******************************************************************************/
+
+
+//#include "stdafx.h"
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+
+using namespace std;
+
+int main()
+{    int r1=2, c1=2, r2=2, c2=2, i, j, k,temp;
+    int a[][2]={ { 2, 4 }, { 3, 4 } };int b[][2]={ { 1, 2 }, { 1, 3 } }; int C[r1][c2];
+
+//int mult[r1][c2]; 
+
+
+    // Initializing elements of matrix mult to 0.
+   while (c1!=r2)
+    {
+        cout << "Error! column of first matrix not equal to row of second.";
+} 
+
+ for(i = 0; i < r1; ++i)
+        for(j = 0; j < c2; ++j)
+        {
+            C[i][j]=0;
+        }
+
+    // Multiplying matrix a and b hence the product in c.
+     for(j = 0; j < c2; ++j)//the matrix will multiply colomn wise
+    for(i = 0; i < r1; ++i)
+        //for(j = 0; j < c2; ++j)
+       // temp=a[i][k];
+            for(k = 0; k < c1; ++k)
+            {
+                
+                C[i][j] += a[i][k] * b[k][j];
+            }
+
+    // Displaying the multiplication of  matrix A and B.
+    cout << endl << "final Matrix: " << endl;
+    for(i = 0; i < r1; ++i)
+    for(j = 0; j < c2; ++j)
+    {
+        cout << " " << mult[i][j];
+        if(j == c2-1)
+            cout << endl;
+    }
+//cout<<product[i][j]<<" ";
+ //              cout<<endl;
+
+
+    return 0;
+}
