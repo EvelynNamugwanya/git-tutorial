222b3194fec73d2bd628c0898691d4b165bfb6ff
EvelynNamugwanya <50854828+EvelynNamugwanya@users.noreply.github.com>
2020-01-10 22:24:56 -0600

    MPI_spawn_function

--- spawn.c
+++ spawn.c
@@ -16,17 +16,17 @@ MPI_Comm intercomm;
 MPI_Init(&argc,&argv);//starr mpi
 
 MPI_Comm_get_parent(&parentcomm);//remote access memory
-if(parentcomm==MPI_COMM_NULL)//WHAT DOES MPI_COMM_NULL MEAN?
+if(parentcomm==MPI_COMM_NULL)//Processes that are not part of the communicator
 
 { 
 //Create 2 more processess 
-MPI_Comm_spawn("spawn_example",MPI_ARGV_NULL/*like length*/,np,MPI_INFO_NULL,0,MPI_COMM_WORLD,&intercomm,errcodes);//MPI_ARGV_NULL- like length of the argument- Passing an argv of MPI_ARGV_NULL to MPI_COMM_SPAWN results in main receiving argc of 1 and an argv whose element 0 is (conventionally) the name of the program.
+MPI_Comm_spawn("spawn",MPI_ARGV_NULL/*like length*/,np,MPI_INFO_NULL,0,MPI_COMM_WORLD,&intercomm,errcodes);//MPI_ARGV_NULL- like length of the argument- Passing an argv of MPI_ARGV_NULL to MPI_COMM_SPAWN results in main receiving argc of 1 and an argv whose element 0 is (conventionally) the name of the program.
 
 
 printf("I'm the parent.\n");
 }else
 {
-printf("iam a parent.\n");
+printf("iam spawned child process\n");
 }
 
 fflush(stdout);//to make the operating system fush any buffers of the underlieing files.//used to clear the buffer 
