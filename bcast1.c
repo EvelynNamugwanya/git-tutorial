e26be5ee555b80f10015dcd418c31670dd0a917a
evelyn <you@example.com>
2020-04-16 19:26:37 -0500

    broadcasting

--- bcast1.c
+++ bcast1.c
@@ -10,7 +10,7 @@ MPI_Init(&argc,&argv);
 MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 MPI_Comm_size(MPI_COMM_WORLD,&size);
 MPI_Datatype data_type;
-size= 6;
+size= 3;
 //array = (int *)malloc(size*size*sizeof(int));
 if(rank==0)
 {
@@ -20,7 +20,17 @@ for(i=0;i<size;i++) 	{
 	array[i][j]=i*size+j;
            //t++; 
 		    }
-			 }
+			
+ }
+printf("process 0z matrix b4 broadcasting");
+
+for (i= 0; i <size;i++)
+{
+for(j= 0; j < size; j++)
+printf("%d [%d]\t",array[i][j],rank);
+printf("\n");
+}
+
 }
 MPI_Bcast(array,size,MPI_INT,0,MPI_COMM_WORLD);
 MPI_Barrier(MPI_COMM_WORLD);
